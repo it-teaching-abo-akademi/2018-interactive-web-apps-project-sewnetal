60056a87745b75ea930720580c41c5de56c7e4ab
Gebremedhin Sewnet Alemseged <Sewnet@Gebremedhins-MacBook-Air.local>
2019-01-01 16:55:55 +0200

    Updates

---
+++ precache-manifest.ec8c6fece192b7a260552cb57aad56a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d269560a5f3bfdd00606",
+    "url": "/2018-interactive-web-apps-project-sewnetal/static/js/runtime~main.d269560a.js"
+  },
+  {
+    "revision": "2ca26e27609971970e65",
+    "url": "/2018-interactive-web-apps-project-sewnetal/static/js/main.2ca26e27.chunk.js"
+  },
+  {
+    "revision": "ee18b6f57b262e1d8e20",
+    "url": "/2018-interactive-web-apps-project-sewnetal/static/js/1.ee18b6f5.chunk.js"
+  },
+  {
+    "revision": "2ca26e27609971970e65",
+    "url": "/2018-interactive-web-apps-project-sewnetal/static/css/main.7d8433c9.chunk.css"
+  },
+  {
+    "revision": "f1c646a43845739fd17ba766887cf4d2",
+    "url": "/2018-interactive-web-apps-project-sewnetal/index.html"
+  }
+];
